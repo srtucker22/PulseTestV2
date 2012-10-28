2501cf15c61867d8f05d1544c1fa4abd7578da40
Simon Tucker <srtucker22@gmail.com>
2012-10-27 18:18:07 -0700

    Initial Commit

---
+++ PulseTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PulseTest
+//
+//  Created by Simon Tucker on 10/27/12.
+//  Copyright (c) 2012 Simon Tucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PulseTestTests/PulseTestTests.h
@@ -0,0 +1,13 @@
+//
+//  PulseTestTests.h
+//  PulseTestTests
+//
+//  Created by Simon Tucker on 10/27/12.
+//  Copyright (c) 2012 Simon Tucker. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PulseTestTests : SenTestCase
+
+@end
